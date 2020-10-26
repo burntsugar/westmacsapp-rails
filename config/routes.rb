df6e359b569333ddee93194e6524ac6a62daacd0
burntsugar <rrakky89@gmail.com>
2020-10-26 12:11:44 +1100

    nest Observation routes in TrailSite

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,14 @@
 Rails.application.routes.draw do
   get 'trail_sites/index'
   get 'start_page/index'
-  #  get 'observation/new'
-  # get 'observation/edit'
-  # get 'observation/update'
-  # get 'observation/create'
-  # get 'observation/show'
-  # get 'observation/destroy'
-
-  resources :observations
-
 
   resources :trail_sites
 
+  # Nest observation routes.
   resources :trail_sites do
     resources :observations
   end
 
-
   root 'start_page#index'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Set a root
 end
