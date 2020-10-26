c9a6c9d3e34c37035eae72ee6244edfb155bc800
burntsugar <rrakky89@gmail.com>
2020-10-26 12:10:40 +1100

    additional model validations

--- app/models/trail_site.rb
+++ app/models/trail_site.rb
@@ -2,6 +2,12 @@
 class TrailSite < ApplicationRecord
     has_many :observations, as: :loggable
 
+    validates_inclusion_of :is_trail_head, presence: true, in: [true, false]
+
+    validates :section_number, presence: true, numericality: { greater_than: 0 }
+
+    validates :distance_from_trail_start, presence: true, numericality: { greater_than: 0 }
+
     after_validation :log_errors, :if => Proc.new {|m| m.errors}
 
     def log_errors
