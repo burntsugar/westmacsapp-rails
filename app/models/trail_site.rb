7f653276f2af6a8a32b7c5a002908b22fb69d39d
burntsugar <rrakky89@gmail.com>
2020-10-26 20:20:24 +1100

    Add validations

--- app/models/trail_site.rb
+++ app/models/trail_site.rb
@@ -1,16 +1,38 @@
 
 class TrailSite < ApplicationRecord
     has_many :observations, as: :loggable
-
+    # is_trail_head
     validates_inclusion_of :is_trail_head, presence: true, in: [true, false]
-
+    # section_number
     validates :section_number, presence: true, numericality: { greater_than: 0 }
-
+    # distance_from_trail_start
     validates :distance_from_trail_start, presence: true, numericality: { greater_than: 0 }
+    # name
+    validates :name, presence: true, length: {minimum: 1} 
+    # description
+    validates :description, presence: true, length: {minimum: 1}
+    # latitude
+    validates :latitude, numericality: { only_float: true, :greater_than => 0, allow_nil: true}
+    # longitude
+    validates :longitude, numericality: { only_float: true, :greater_than => 0, allow_nil: true}
+    # elevation
+    validates :elevation, numericality: { only_integer: true, allow_nil: true }
 
     after_validation :log_errors, :if => Proc.new {|m| m.errors}
 
     def log_errors
         Rails.logger.debug self.errors.full_messages.join("\n")
+    end
+
+    after_initialize :init
+  
+    private
+    def init
+        if self.new_record? && self.elevation.nil?
+            self.elevation = 0
+        end
+        # TODO: is_trail_head false
+        # TODO: latitude 0
+        # TODO: longitude 0
     end
 end
