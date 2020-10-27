54de40690a838bf50cdc5b772a7f36ae88b594a0
burntsugar <rrakky89@gmail.com>
2020-10-27 20:03:20 +1100

    refactor and robust

--- spec/models/trail_site_spec.rb
+++ spec/models/trail_site_spec.rb
@@ -1,16 +1,39 @@
 require 'rails_helper'
 
+
+
 RSpec.describe TrailSite, :type => :model do
+
+  before(:each) do
+    @h_master = {is_trail_head: true, 
+      section_number: 1, 
+      distance_from_trail_start: Rails.configuration.custom_trail.num_of_sections,
+      name: "xxx",
+      description: "xxx",
+      latitude: 1,
+      longitude: 1,
+      elevation: 1}
+      puts @h_master
+      @h_master
+  end
+
+  it "is valid with all valid values" do
+    trail_site = TrailSite.new(@h_master)
+    expect(trail_site).to be_valid
+  end
   it "is not valid without a trail head status" do
-    trail_site = TrailSite.new(is_trail_head: nil, section_number: 1, distance_from_trail_start: 10)
+    @h_master[:is_trail_head]=nil
+    trail_site = TrailSite.new(@h_master)
     expect(trail_site).to_not be_valid
   end
   it "is not valid without a valid section number" do
-    trail_site = TrailSite.new(is_trail_head: true, section_number: -10, distance_from_trail_start: 10)
+    @h_master[:section_number]=-10
+    trail_site = TrailSite.new(@h_master)
     expect(trail_site).to_not be_valid
   end
   it "is not valid without a description" do
-    trail_site = TrailSite.new(is_trail_head: true, section_number: 1, distance_from_trail_start: -10)
+    @h_master[:distance_from_trail_start]=-10
+    trail_site = TrailSite.new(@h_master)
     expect(trail_site).to_not be_valid
   end
   it "is not valid without a start_date"
