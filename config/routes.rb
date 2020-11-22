5fbdef6c3bee2ebf08315c66117bf8e279c88bb7
burntsugar <rrakky89@gmail.com>
2020-11-22 23:42:00 +1100

    nest Observation resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
+# /config/routes.rb
 Rails.application.routes.draw do
-  get 'trail_sites/index'
-  get 'start_page/index'
-
-  resources :trail_sites
-
-  # Nest observation routes.
   resources :trail_sites do
-    resources :observations
+    resources :observations, module: :trail_sites
   end
-
+  resources :facility do
+    resources :observations, module: :facility
+  end
   root 'start_page#index'
-
 end
