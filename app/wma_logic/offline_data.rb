8d9aa7824ef6c0d4a4c1e580cd070003db9f7feb
burntsugar <rrakky89@gmail.com>
2020-11-24 16:04:42 +1100

    progressing with offline data deserialisation

--- app/wma_logic/offline_data.rb
+++ app/wma_logic/offline_data.rb
@@ -1,31 +1,49 @@
 module OfflineData 
     
     @@file = File.join(Rails.root, 'app', 'wma_logic', 'new_data.json')
-    file_string = File.read(@@file)
+    @@file_string = File.read(@@file)
 
-    data_hash = ActiveSupport::JSON.decode(file_string)
-
-    data_array = []
-    data_hash['trailsites'].each do |key,value|
-        key.delete('other_names')
-        key.delete('coords')
-        key.delete('observations')
-        key.delete('facilities')
-        my_object = TrailSite.new(key)
-        data_array << my_object
+    def OfflineData::make_data_hash
+        ActiveSupport::JSON.decode(@@file_string)
     end
 
-    data_hash['campsites'].each do |key,value|
-        key.delete('other_names')
-        key.delete('coords')
-        key.delete('observations')
-        key.delete('facilities')
-        my_object = TrailSite.new(key)
-        data_array << my_object
+    @@main_obj_array = []
+
+    def OfflineData::make_trail_sites()
+        dh = make_data_hash
+        h = dh['trailsites'] + dh['campsites']
+        h.each do |key,value|
+            key.delete('other_names')
+            key.delete('coords')
+            key.delete('observations')
+            key.delete('facilities')
+            my_object = TrailSite.new(key)
+            @@main_obj_array << my_object
+        end
     end
 
-    data_array.each do |o|
-        o.save
+    def OfflineData::save_trail_sites
+        @@main_obj_array.each do |o|
+            o.save
+        end
     end
+
+    def OfflineData::process_trailsite_observations()
+        dh = make_data_hash
+        h = dh['trailsites'] + dh['campsites']
+        h.each do |key,value|
+            @trail_site = TrailSite.find_by(name: key['name'])
+            @observations = @trail_site.observations.build
+            key['observations'].each do |o|
+                @observation = @trail_site.observations.new(o)
+                @observation.save
+            end
+        end
+         
+    end
+
+    make_trail_sites
+    save_trail_sites
+    process_trailsite_observations
     
 end
