118adbad0dcd4376e89ff6f2444b9006b2b768f1
burntsugar <rrakky89@gmail.com>
2020-11-24 18:41:48 +1100

    Changed assertion from .new to .create

--- spec/models/trail_site_spec.rb
+++ spec/models/trail_site_spec.rb
@@ -11,100 +11,100 @@ RSpec.describe TrailSite, :type => :model do
       latitude: Rails.configuration.custom_trail.bbox_max_lat,
       longitude: Rails.configuration.custom_trail.bbox_max_lng,
       elevation: Rails.configuration.custom_trail.max_elevation}
-      puts @h_master
-      @h_master
   end
 
   it "is valid with all valid values" do
-    trail_site = TrailSite.new(@h_master)
+    trail_site = TrailSite.create(@h_master)
     expect(trail_site).to be_valid
+  end
+  it "is created with all valid values" do
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to be_empty
   end
   it "is not valid without a trail head status" do
     @h_master[:is_trail_head]=nil
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid without a section number" do
     @h_master.delete(:section_number)
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid without a positive section number" do
     @h_master[:section_number]=-10
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid without a section number in the correct range" do
     @h_master[:section_number]=Rails.configuration.custom_trail.num_of_sections + 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid without trail distance from start" do
     @h_master[:distance_from_trail_start]=nil
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid when exceeds max trail distance from start" do
     @h_master[:distance_from_trail_start]=Rails.configuration.custom_trail.max_distance_from_trail_start + 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid when trail distance from start < 0" do
     @h_master[:distance_from_trail_start]=-1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid when a name is not given" do
     @h_master[:name]=""
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with a nill description" do
     @h_master[:description]=nil
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with a zero length description" do
     @h_master[:description]=""
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with description length >= 256" do
     @h_master[:description]="B"*256
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with latitude exceeding max" do
     @h_master[:latitude]=Rails.configuration.custom_trail.bbox_max_lat + 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with latitude less than min" do
     @h_master[:latitude]=Rails.configuration.custom_trail.bbox_min_lat - 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with longitude exceeding max" do
     @h_master[:longitude]=Rails.configuration.custom_trail.bbox_max_lng + 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with longitude exceeding max" do
     @h_master[:longitude]=Rails.configuration.custom_trail.bbox_min_lng - 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with elevation exceeding max" do
     @h_master[:elevation]=Rails.configuration.custom_trail.max_elevation + 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
   it "is not valid with elevation less than min" do
     @h_master[:elevation]=Rails.configuration.custom_trail.min_elevation - 1
-    trail_site = TrailSite.new(@h_master)
-    expect(trail_site).to_not be_valid
+    trail_site = TrailSite.create(@h_master)
+    expect(trail_site.errors).to_not be_empty
   end
 
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
 end
